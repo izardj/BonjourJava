f114acd6df5564c568cfe9f0ca409fd45d575c37
izardj <izard.jerome@free.fr>
2017-03-21 09:31:31 +0100

    final

---
+++ src/premierProgrammeJava/Bonjour.java
@@ -0,0 +1,18 @@
+package premierProgrammeJava;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// Commentaire
+		
+		/* Commentaire sur plusieur lignes
+		 * lignes 2
+		 * ligne 3
+		 * */
+		
+		/**
+		 * commentaire javadoc
+		 */
+		System.out.println("Bonjour JAVA!!");
+	}
+}
